e1a34a8288e7f7ba7fac32ac84b3aa4f2c93b5fe
luis castillo <luiscastillo@iwebsapp.com>
2018-05-25 02:59:14 -0500

    animación en el lanch screen

--- school/LoginController.swift
+++ school/LoginController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import RevealingSplashView
 
 class LoginController: UIViewController {
 
+    let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "icon-sanus")!, iconInitialSize: CGSize(width: 161, height: 127), backgroundColor: UIColor.init(red: 245/255, green: 246/255, blue: 250/255, alpha: 1.0))
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(revealingSplashView)
+        self.revealingSplashView.animationType = SplashAnimationType.popAndZoomOut
+        self.revealingSplashView.startAnimation()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
