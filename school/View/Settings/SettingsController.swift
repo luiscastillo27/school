6b75c3b1dde9c36ce79b699de3a2f4d3ff3d9b07
luis castillo <luiscastillo@iwebsapp.com>
2018-05-26 01:01:12 -0500

    areglando interfaz  ajustes

--- school/View/Settings/SettingsController.swift
+++ school/View/Settings/SettingsController.swift
@@ -25,11 +25,11 @@ class SettingsController: UIViewController {
         listenerEditUser.layer.shadowColor = UIColor.black.cgColor
         listenerEditUser.layer.shadowOpacity = 0.5
         listenerEditUser.layer.shadowOffset = CGSize.zero
-        
+
         cardViewState.layer.shadowColor = UIColor.black.cgColor
         cardViewState.layer.shadowOpacity = 0.5
         cardViewState.layer.shadowOffset = CGSize.zero
-        
+
         listenerExit.layer.shadowColor = UIColor.black.cgColor
         listenerExit.layer.shadowOpacity = 0.5
         listenerExit.layer.shadowOffset = CGSize.zero
