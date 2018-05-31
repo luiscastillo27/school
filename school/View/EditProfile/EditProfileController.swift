79be3d2df82f3491050f341faa2ba78b4d365216
luis castillo <luiscastillo@iwebsapp.com>
2018-05-31 17:38:03 -0500

    interfaz editar perfil

---
+++ school/View/EditProfile/EditProfileController.swift
@@ -0,0 +1,21 @@
+//
+//  EditProfileController.swift
+//  school
+//
+//  Created by admin on 31/05/18.
+//  Copyright © 2018 iwebsapp. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func dissmis(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil )
+    }
+
+}
